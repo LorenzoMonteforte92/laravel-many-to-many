d31924f5a3561b578ffa66cd3638b1746c487c36
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-06-06 14:15:05 +0200

    fix: relations

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,14 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Technology;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'slug', 'client_name', 'summary', 'image', 'type_id'];
+    protected $fillable = ['name', 'slug', 'client_name', 'summary', 'image', 'type_id',];
 
     public function type() {
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies() {
+        return $this->belongsToMany(Technology::class);
     }
 }
